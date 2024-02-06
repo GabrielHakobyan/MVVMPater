56f040e64124cfed00794504114928cfcc26c781
GabrielHakobyan <hakobyan.gabriel@mail.ru>
2024-02-06 14:29:46 +0400

    Gabriel

---
+++ MVVMPattern/Model/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVVMPattern.Model.Data
+{
+    class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Databas=gabrielDB;Trusted_Connection=True;");
+        }
+
+    }
+}
---
+++ MVVMPattern/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMPattern.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Phone { get; set; }
+        public int PositionId { get; set; }
+        public Position Position { get; set; }
+
+
+    }
+}
